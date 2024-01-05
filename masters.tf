53e0f04538082eff0ae36d0b5465067b8ae35da6
Alexander A. Klimov <alexander.klimov@icinga.com>
2024-01-05 15:40:10 +0100

    Centos 7

--- masters.tf
+++ masters.tf
@@ -1,7 +1,7 @@
 resource "openstack_compute_instance_v2" "master" {
 	for_each = toset([for i in range(1, 3) : tostring(i)])
 	name = "${var.vm_prefix}master-${each.value}"
-	image_name = "Debian 12"
+	image_name = "Centos 7"
 	flavor_name = "s1.xxlarge"
 	network { name = "${var.vm_network}" }
 	security_groups = [ "default" ]
